1b153ec5be7d0f614ba32583b7380f3158445245
Patrick Bryan <pbryan9@gmail.com>
2023-07-19 14:45:54 -0700

    move api routes to separate module

--- server/api/apiRoute.cjs
+++ server/api/apiRoute.cjs
@@ -1,27 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { User } = require('../db/index.cjs');
-const { requireToken } = require('../middleware.cjs');
-
-const userRouter = require('./user/userRoute.cjs');
-router.use('/user', userRouter);
-
-router.post('/auth', async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    res.send({ token: await User.authenticate({ username, password }) });
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get('/auth', requireToken, (req, res, next) => {
-  try {
-    res.send(req.user);
-  } catch (err) {
-    next(err);
-  }
-});
+router.use('/user', require('./user/userRoute.cjs'));
+router.use('/auth', require('./auth/authRoute.cjs'));
 
 module.exports = router;
