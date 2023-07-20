9d66dfc86577061b076c698adee529c64ed1213d
ruralFluffyDill <stacylukavsky@gmail.com>
2023-07-20 13:50:22 -0400

    add signup route

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@ export { default as Navbar } from './Navbar.jsx';
 export { default as Signin } from './Signin.jsx';
 export { default as AudioVideoControls } from './AudioVideoControls.jsx';
 export { default as Chat } from './Chat.jsx';
+export { default as SignUp } from './SignUp.jsx';
