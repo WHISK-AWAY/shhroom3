a2e168565664ca478731df2df345c513f3610bbc
ruralFluffyDill <stacylukavsky@gmail.com>
2023-07-20 13:50:54 -0400

    gutted lobby from existing rooms

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import verifyToken from '../lib/utils';
 
 export default function Lobby({ socket }) {
-  const [rooms, setRooms] = useState([]);
+  // const [rooms, setRooms] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,9 +15,9 @@ export default function Lobby({ socket }) {
     });
   }, []);
 
-  socket.on('room-list', (roomList) => {
-    setRooms(roomList);
-  });
+  // socket.on('room-list', (roomList) => {
+  //   setRooms(roomList);
+  // });
 
   return (
     <div className='signin-form flex flex-col justify-center gap-7 shadow-lg shadow-gray-800/60  items-center min-w-min max-w-sm mx-auto mt-40 bg-gradient-to-r from-pink-500/50 via-purple-500/60 to-indigo-500/60 rounded text-slate-200 h-96'>
@@ -27,20 +27,6 @@ export default function Lobby({ socket }) {
       >
         New Meeting
       </Link>
-      <br />
-      {rooms.map((room, idx) => {
-        return (
-          <React.Fragment key={room}>
-            <Link
-              to={`/room/${room}`}
-              className='inline-block bg-gradient-to-t from-violet-700 to-slate-300  hover:shadow-dark-pink4/40 py-3 px-5 rounded-xl shadow-md shadow-gray-800/60 transition duration-500 hover:scale-105 font-medium tracking-wide'
-            >
-              Join Existing Room {idx + 1}
-            </Link>
-            <br />
-          </React.Fragment>
-        );
-      })}
     </div>
   );
 }
