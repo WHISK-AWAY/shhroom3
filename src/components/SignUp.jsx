394b6cd237fbc8efb2819ff8b7eeb463799f0fcf
ruralFluffyDill <stacylukavsky@gmail.com>
2023-07-20 18:49:55 -0400

    validation finnally works

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,44 +1,45 @@
 import { useForm } from 'react-hook-form';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import z from 'zod';
-import {zodResolver} from '@hookform/resolvers/zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import axios, { AxiosError } from 'axios';
 const VITE_API_URL = import.meta.env.VITE_API_URL;
-
 
 const ZSignUp = z
   .object({
     username: z
       .string()
-      .min(2, { message: 'must be minumum of 2 characters' })
-      .max(15, { message: 'must be maximum of 15 characters' }),
+      .min(2, { message: 'must contain minumum of 2 characters' })
+      .max(15, { message: 'must contain maximum of 15 characters' }),
     password: z
       .string()
-      .min(8, { message: 'must be minimum of 8 characters' })
-      .max(20, { message: 'must be maximum of 20 characters' }),
+      .min(8, { message: 'must contain minimum of 8 characters' })
+      .max(20, { message: 'must contain maximum of 20 characters' }),
     confirmPassword: z
       .string()
-      .min(8, { message: 'must be minimum of 8 characters' })
-      .max(20, { message: 'must be maximum of 20 characters' }),
+      .min(8, { message: 'must contain minimum of 8 characters' })
+      .max(20, { message: 'must contain maximum of 20 characters' }),
   })
   .strict()
-  .refine(
-    ({ password, confirmPassword }) => {
-      return password === confirmPassword;
-    },
-    { message: 'password does not match', path: 'confirmPassword' },
-  );
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (password !== confirmPassword) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'password fields do not match',
+        path: ['confirmPassword'],
+      });
+    }
+  });
 
 export default function SignUp() {
-
   const navigate = useNavigate();
+  const [isFormSubmitted, setFormSubmitted] = useState(false);
 
   const {
     register,
     handleSubmit,
-    reset,
-    setErrors,
+    resetField,
     formState: { errors },
   } = useForm({
     resolver: zodResolver(ZSignUp),
@@ -47,47 +48,46 @@ export default function SignUp() {
       password: '',
       confirmPassword: '',
     },
+    mode: 'onBlur',
   });
 
+  const submitFormData = async (data) => {
+    if (!data) return;
 
-  const submitFormData = async(data) => {
-    if(!data) return ;
+    try {
+      const { data: dataPayload } = await axios.post(
+        `${VITE_API_URL}/api/user`,
+        data,
+      );
 
-    try{
-      const {data: dataPayload} = await axios.post(`${VITE_API_URL}/api/user`, data);
-
-
-      if(dataPayload.token)
-       localStorage.setItem('token', dataPayload.token);
+      if (dataPayload.token) localStorage.setItem('token', dataPayload.token);
       navigate('/lobby');
 
-
-      console.log('dp', dataPayload)
       return dataPayload;
-
-
-    }catch(err){
-      if( err instanceof AxiosError) {
+    } catch (err) {
+      if (err instanceof AxiosError) {
         throw new Error(err);
       } else {
         console.error(err);
       }
+  
     }
-  }
+  };
+
+
 
 useEffect(() => {
-  if(errors.confirmPassword) {
-    reset(
-      {
-        password: '',
-        confirmPassword: ''
-      },
-      {
-        keepErrors: true,
-      }
-    )
+  for (let key in errors) {
+
+
+    if(key === 'confirmPassword') {
+    resetField('password', {keepDirty: false});
+    resetField('confirmPassword', {keepDirty: false, keepError: true});
+  
+    }
   }
-}, [errors.confirmPassword])
+}, [errors.username, errors.confirmPassword])
+
 
   return (
     <div className='sign-up-wrapper flex justify-center pt-20'>
@@ -100,28 +100,28 @@ useEffect(() => {
         <input
           type='text'
           id='username'
-          placeholder={errors.username?.message || ''}
           className='text-black'
           {...register('username')}
         />
+        <p>{errors.username?.message || ''}</p>
 
         <label htmlFor='password'>password</label>
         <input
           type='password'
           id='password'
-          placeholder={errors.password?.message || ''}
           className='text-black'
           {...register('password')}
         />
+        <p>{errors.password?.message || ''}</p>
 
         <label htmlFor='confirm-password'>confirm password</label>
         <input
           type='password'
           id='confirm-password'
-          placeholder={errors.confirmPassword?.message || ''}
           className='text-black'
           {...register('confirmPassword')}
         />
+        {errors.confirmPassword?.message || ''}
 
         <button type='submit'>sign up</button>
       </form>
