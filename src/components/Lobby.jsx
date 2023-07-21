df5a6c642ca0e46df1432fff4755f5ba44693a83
Patrick Bryan <pbryan9@gmail.com>
2023-07-21 14:14:03 -0700

    brought back room list for testing

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import useVerifyToken from './hooks/useVerifyToken';
 
 export default function Lobby({ socket }) {
-  // const [rooms, setRooms] = useState([]);
+  const [rooms, setRooms] = useState([]);
   const navigate = useNavigate();
 
   const tokenStatus = useVerifyToken();
@@ -19,9 +19,9 @@ export default function Lobby({ socket }) {
     }
   }, [tokenStatus.loading]);
 
-  // socket.on('room-list', (roomList) => {
-  //   setRooms(roomList);
-  // });
+  socket.on('room-list', (roomList) => {
+    setRooms(roomList);
+  });
 
   return (
     <div className='signin-form flex flex-col justify-center gap-7 shadow-lg shadow-gray-800/60  items-center min-w-min max-w-sm mx-auto mt-40 bg-gradient-to-r from-pink-500/50 via-purple-500/60 to-indigo-500/60 rounded text-slate-200 h-96'>
@@ -31,6 +31,15 @@ export default function Lobby({ socket }) {
       >
         New Meeting
       </Link>
+      {rooms.length > 0 &&
+        rooms.map((room, idx) => (
+          <Link
+            to={`/room/${room}`}
+            className='inline-block bg-gradient-to-t from-violet-700 to-slate-300  hover:shadow-dark-pink4/40 py-3 px-5 rounded-xl shadow-md shadow-gray-800/60 transition duration-500 hover:scale-105 font-medium tracking-wide'
+          >
+            Existing room {idx + 1}
+          </Link>
+        ))}
     </div>
   );
 }
