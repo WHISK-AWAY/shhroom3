d0534a74861ed2b3272a6d9ad5788618195c0f97
Patrick Bryan <pbryan9@gmail.com>
2023-07-21 13:46:27 -0700

    shift socket server to dedicated file

--- server/server.cjs
+++ server/server.cjs
@@ -1,21 +1,16 @@
 require('dotenv').config();
+require('./sockets.cjs');
 const CLIENT_URL = process.env.CLIENT_URL;
+const API_PORT = process.env.API_PORT || 3000;
 
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const server = require('http').Server(app);
-const path = require('path');
 
 const { ZodError } = require('zod');
 const { fromZodError } = require('zod-validation-error');
 
-const io = require('socket.io')(server, {
-  cors: {
-    origin: CLIENT_URL,
-  },
-});
-
+// Basic server setup
 app.use(express.static('public'));
 app.use(express.json());
 app.use(
@@ -24,11 +19,8 @@ app.use(
   }),
 );
 
+// API route entry point
 app.use('/api', require('./api/apiRoute.cjs'));
-
-// app.use('*', (req, res, next) => {
-//   res.status(200).sendFile(path.join(__dirname, '../public/index.html'));
-// });
 
 // * Zod validation error handler
 app.use((err, req, res, next) => {
@@ -40,46 +32,17 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-const roomList = [];
-
-io.on('connection', (socket) => {
-  socket.on('room-created', (roomId) => {
-    roomList.push(roomId);
-    console.log('server heard "room connected"');
-    io.emit('room-list', roomList);
-  });
-
-  console.log('hello form server');
-
-  socket.on('new-lobby', () => {
-    console.log('new lobby joined');
-    socket.emit('room-list', roomList);
-  });
-
-  socket.on('join-room', (params) => {
-    const { peerId, roomId, userId, publicKey, username } = params;
-    console.log('join-room received');
-    socket.join(roomId);
-    socket.to(roomId).emit('user-connected', peerId, userId, publicKey);
-
-    socket.on('disconnect', () => {
-      socket.to(roomId).emit('user-disconnected', peerId, userId);
-    });
-    socket.on('leave-room', () => {
-      socket.to(roomId).emit('user-disconnected', peerId, userId);
-    });
-  });
-});
-
-async function init() {
+function init() {
   console.log('Allowing CORS traffic from ' + CLIENT_URL);
 
-  server.listen(3002, () => {
-    console.log('WS server listening at', 3002);
-  });
-  app.listen(3000, () => {
-    console.log('HTTP server listening at', 3000);
+  // server.listen(3002, () => {
+  //   console.log('WS server listening at', 3002);
+  // });
+  app.listen(API_PORT, () => {
+    console.log('HTTP server listening at', API_PORT);
   });
 }
+
+module.exports = app;
 
 init();
---
+++ server/sockets.cjs
@@ -0,0 +1,51 @@
+const CLIENT_URL = process.env.CLIENT_URL;
+const WS_PORT = process.env.WS_PORT || 3002;
+
+const expressApp = require('./server.cjs');
+const server = require('http').Server(expressApp);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: CLIENT_URL,
+  },
+});
+
+const roomList = [];
+
+io.on('connection', (socket) => {
+  socket.on('room-created', (roomId) => {
+    roomList.push(roomId);
+    console.log('server heard "room connected"');
+    io.emit('room-list', roomList);
+  });
+
+  // this isn't necessary, but I've grown to like seeing it
+  console.log('hello form server');
+
+  socket.on('new-lobby', () => {
+    console.log('new lobby joined');
+    socket.emit('room-list', roomList);
+  });
+
+  socket.on('join-room', (params) => {
+    const { peerId, roomId, userId, publicKey, username } = params;
+    console.log('join-room received');
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', peerId, userId, publicKey);
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', peerId, userId);
+    });
+    socket.on('leave-room', () => {
+      socket.to(roomId).emit('user-disconnected', peerId, userId);
+    });
+  });
+});
+
+function init() {
+  server.listen(WS_PORT, () => {
+    console.log('WS server listening at', WS_PORT);
+  });
+}
+
+init();
