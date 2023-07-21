d83cadcc2a9f48d4dbe22225cd0288812edfae0d
Patrick Bryan <pbryan9@gmail.com>
2023-07-21 13:46:40 -0700

    remove unused dep

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import verifyToken from '../lib/utils';
 import useVerifyToken from './hooks/useVerifyToken';
 
 export default function Lobby({ socket }) {
