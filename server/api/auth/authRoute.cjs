c72ea7a941c3a378e37ae4d278ae97a856bbaa97
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-03 12:42:51 -0400

    add username check

--- server/api/auth/authRoute.cjs
+++ server/api/auth/authRoute.cjs
@@ -7,7 +7,24 @@ const { requireToken } = require('../../middleware.cjs');
 router.post('/', async (req, res, next) => {
   try {
     const { username, password } = req.body;
-    res.json({ token: await User.authenticate({ username, password }) });
+
+    console.log('unaem', req.body)
+
+    const usernameCheck= await User.findOne({ where: { username:username}});
+    if (!usernameCheck || usernameCheck === null) {
+
+      
+      console.log('use4rCherck', usernameCheck)
+      return res.status(404).send({ message: 'given username does not exist in the database' });
+    }
+
+
+      const authCheck = await User.authenticate({ username, password });
+
+      if(!authCheck) {
+       return res.status(401).send({message: 'invalid credentials'});
+      }
+    res.status(200).json({ token: authCheck });
   } catch (err) {
     next(err);
   }
