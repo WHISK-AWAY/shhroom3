66a55af8896ae0cb5b278249d968f708c2de0e6d
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-03 16:05:03 -0400

    centered txt

--- src/components/Screensaver.jsx
+++ src/components/Screensaver.jsx
@@ -3,17 +3,16 @@ import compIcon from '/svg/computerIcon.svg';
 
 export default function Screensaver() {
   return (
-    <div className='bg-[#5DC0EA]'>
+    <div className='bg-[#5DC0EA] text-[#151521]'>
       <div
-        className={`w-screen h-screen bg-contain bg-no-repeat bg-bottom
+        className={`w-screen h-screen bg-contain bg-no-repeat bg-bottom 
     bg-[url('/bg/screen_saver1.jpg')]`}
       >
-        <div className='relative pt-[19%] px-[10%] mx-[20%] flex flex-col items-end border'>
-          <img src={compIcon} alt='' className='h-20 w-20  ' />
-          <p className='font-vt text-[1.5vw] pl-[60%]'>sign_in/sign_up</p>
+        <div className='relative pt-[19%] px-[10%] mr-[10%] flex flex-col items-end  '>
+          <img src={compIcon} alt='old computer screen icon' className='h-[12%] w-[12%] drop-shadow-xl  ' />
+          <p className='font-vt text-[1.5vw] pt-[1%] relative translate-x-[5%]'>sign_in/sign_up</p>
         </div>
       </div>
-      
     </div>
   );
 }
