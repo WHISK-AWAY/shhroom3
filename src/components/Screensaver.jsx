72c6781b7542fb0aa22a485db28be21d7c52dbf4
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-04 19:25:41 -0400

    add zoom clicker to screensaver

--- src/components/Screensaver.jsx
+++ src/components/Screensaver.jsx
@@ -4,12 +4,40 @@ import Signin from './Signin';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SignUp from './SignUp';
+import { useContext, useMemo } from 'react';
+import { ZoomContext } from './Landing/Landing';
+import { Vector3 } from 'three';
+
 
 export default function Screensaver() {
   const [isFormHidden, setIsFormHidden] = useState(true);
   const [isSignUpHidden, setIsSignUpHidden] = useState(true);
+  const zoom = useContext(ZoomContext);
+
+
+    const monitorZoomPosition = useMemo(
+      () => new Vector3(3.54909, 3.20587, 2.15376),
+    );
+
+
+    function zoomClicker(e) {
+      // if we're already zoomed in, don't do anything
+      if (zoom.zoomMode) {
+        e.stopPropagation();
+      } else {
+        zoom.setZoom((prev) => ({
+          ...prev,
+          targetPosition: monitorZoomPosition,
+          targetLabel: 'monitor',
+          zoomMode: true,
+          controlsEnabled: false,
+        }));
+      }
+    }
+
+
   return (
-    <div className='bg-[#5DC0EA] text-[#151521]'>
+    <div onClick={zoomClicker} className='bg-[#5DC0EA] text-[#151521]'>
       <div
         className={`w-[1600px] h-[990px] bg-cover bg-no-repeat bg-bottom
     bg-[url('/bg/screen_saver1.jpg')]`}
