34ddd850202a5668122251ff86eb9bd6aed8d556
Patrick Bryan <pbryan9@gmail.com>
2023-08-08 17:21:13 -0700

    rename file

--- src/components/Landing/renderNewMeetingText.js
+++ src/components/Landing/renderNewMeetingText.js
@@ -2,11 +2,11 @@ import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import * as THREE from 'three';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { useThree } from '@react-three/fiber';
-export default function font() {
+
+export default function renderNewMeetingText() {
   const state = useThree();
   const text = 'NEW MEETING';
   let textMesh;
- 
 
   const loader = new FontLoader();
   loader.load('/fonts/Train One_Regular.json', function (font) {
