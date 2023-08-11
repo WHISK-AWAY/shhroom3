ed0e712341900bc182625605c37f1e3c49a53cb6
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-11 12:00:36 -0400

    nested ternary to hear change of state

--- src/components/VideoGrid/VideoGrid.jsx
+++ src/components/VideoGrid/VideoGrid.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 import Video from './Video';
 
-export default function VideoGrid({ ownSource, peerSource }) {
+export default function VideoGrid({ ownSource, peerSource, isUserControlsOpen }) {
   const [isFullScreen, setIsFullScreen] = useState(false);
+
 
   return (
     <div
       id='video-grid'
       className={`flex justify-center gap-8 pt-24 ${
         isFullScreen ? 'group is-fullscreen' : ''
-      }`}
+      } ${isUserControlsOpen ? 'w-[90%] flex justify-end items-end self-end align-end ml-[14%]' : ''}`}
     >
       {ownSource?.id && (
         <Video
