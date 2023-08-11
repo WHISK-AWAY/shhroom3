c80f27b53b0cab97d141cf4df5c55beb4fb4faae
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-11 18:30:44 -0400

    add ternary to hear when user controls are open

--- src/components/VideoGrid/VideoGrid.jsx
+++ src/components/VideoGrid/VideoGrid.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Video from './Video';
 
-export default function VideoGrid({ ownSource, peerSource, isUserControlsOpen }) {
+export default function VideoGrid({ ownSource, peerSource, isUserControlsOpen, setIsUserControlsOpen }) {
   const [isFullScreen, setIsFullScreen] = useState(false);
 
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsUserControlsOpen((prev) => !prev)
+  //   }, 2000)
+  // }, [setIsUserControlsOpen])
 
   return (
     <div
       id='video-grid'
-      className={`flex justify-center gap-8 pt-24 ${
+      className={`flex justify-center gap-8 pt-[10%]   ${
         isFullScreen ? 'group is-fullscreen' : ''
-      } ${isUserControlsOpen ? 'w-[90%] flex justify-end items-end self-end align-end ml-[14%]' : ''}`}
+      } ${isUserControlsOpen ? 'w-[80%] flex justify-end items-end self-end align-end ml-[14%] mx-auto' : ''}`}
     >
       {ownSource?.id && (
         <Video
