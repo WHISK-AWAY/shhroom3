0e6444db90b8bc8cf74569006b07033d55c2e7da
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-11 18:28:12 -0400

    style in progress

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -52,18 +52,18 @@ export default function Chat(props) {
   };
 
   return (
-    <div className='chat-area-wrapper max-h-[35vh] flex flex-col justify-end items-center shrink md:shrink-[1] px-5 font-vt text-lg'>
-      <div className='flex flex-row h-full items-center rounded-t-md scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-slate-700 scrollbar-track-slate-800 w-[86vw] mx-auto '>
+    <div className='chat-area-wrapper h-[30%] w-[80%] flex flex-col mx-auto font-vt text-[20px] bg-teal-400/20 rounded-t-md mt-2'>
+      <div className='flex flex-row h-[80%] items-center justify-center rounded-t-md scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-slate-700 scrollbar-track-slate-800 w-full mx-auto'>
         <div
           id='chat-container'
-          className='overflow-auto h-full bg-nav-gradient-dark/50 px-3 text-slate-400 shrink w-full'
+          className='overflow-auto h-full bg-nav-gradient-dark/50 px-3 text-slate-400 w-full'
         >
           {messageList &&
             messageList.map(({ username, timestamp, message }) => (
               <div key={timestamp}>
                 <p>
-                  <span className='font-semibold'>{username} </span>
-                  <span className='text-sm text-slate-600 ml-1'>
+                  <span className='font-semibold text-teal-300'>{username} </span>
+                  <span className='text-sm text-slate-400 ml-1'>
                     {new Date(timestamp).toLocaleString(undefined, {
                       hour: 'numeric',
                       minute: 'numeric',
@@ -71,7 +71,7 @@ export default function Chat(props) {
                     })}
                   </span>
                 </p>
-                <p>{message}</p>
+                <p className='text-slate-300'>{message}</p>
               </div>
             ))}
           <div id='scroll-anchor'></div>
@@ -79,7 +79,7 @@ export default function Chat(props) {
       </div>
       <form onSubmit={handleMessage} className='w-full'>
         <input
-          className='pl-2 py-2 text-white bg-gray-900/70 border-[.5px] border-primary-dark-blue w-full rounded-b-md placeholder-slate-500 focus:outline-none focus:border-2'
+          className='px-2 py-1 text-white bg-teal-500/70 border-[.5px] border-white w-full h-[40px] rounded-b-md placeholder-slate-100 focus:outline-none focus:border-2'
           type='text'
           name='chat'
           id='chat'
