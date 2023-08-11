52f29a5d838191876cbf940fd99047cf28ca7d1c
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-10 23:15:32 -0400

    fixing controls

--- src/components/RoomUserControls.jsx
+++ src/components/RoomUserControls.jsx
@@ -23,33 +23,36 @@ export default function RoomUserControls({
 
 
   return (
-    <div className='room-controls-wrapper absolute top-0 left-0  h-[60%] w-[15%]  font-vt flex z-50  justify-center rounded-sm  text-white'>
-      <div className=' absolute bg-gradient-to-r w-full h-full from-pink-600 to-purple-600 rounded-lg  group-hover:opacity-75 transition duration-1000 group-hover:duration-600 animate-tilt blur -z-10'/>
-        <div className=' backdrop-blur '>
+    <div className='room-controls-wrapper absolute top-0 left-0  h-[60%] w-[15%]  font-vt flex z-50  justify-center rounded-sm  text-white border'>
+
+    <div className=' border-b w-full h-[7%] flex justify-end items-center absolute'>
+    <img src={arrowDown} alt="" className='h-[13px]  '/>
+    </div>
+      
+     
         <ul className='room-controls-list flex flex-col gap-10 justify-center   bg-black h-full p-4 rounded-md'>
-        <img src={arrowDown} alt="" className='h-10'/>
             <button
               className='flex flex-col items-center'
               onClick={() => copyToClipboard(text)}
             >
-              <img src={test} alt='' className='w-20' />
-              <li className='link-invite'>copy invite link to clipboard</li>
+              <img src={test} alt='' className='w-[50%]' />
+              <li className='link-invite text-[10px]'>copy invite link to clipboard</li>
             </button>
 
             <button className='flex flex-col items-center'>
-              <img src={test} alt='' className='w-20' />
-              <li className='link-invite'>chat</li>
+              <img src={test} alt='' className='w-[50%]' />
+              <li className='link-invite text-[10px]'>chat</li>
             </button>
 
             <button
               className='flex flex-col  items-center'
               onClick={leaveMeeting}
             >
-              <img src={test} alt='' className='w-20' />
-              <li className='link-invite'>leave meeting</li>
+              <img src={test} alt='' className='w-[50%]' />
+              <li className='link-invite text-[10px]'>leave meeting</li>
             </button>
           </ul>
-        </div>
+        
       
     </div>
   );
