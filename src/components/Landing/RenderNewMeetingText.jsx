9552a1fe2a5919bad128b1326d84ae522deb4a79
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-19 18:13:16 -0400

    turn into component to lazy load

---
+++ src/components/Landing/RenderNewMeetingText.jsx
@@ -0,0 +1,66 @@
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { Mesh, MeshStandardMaterial } from 'three';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { useThree } from '@react-three/fiber';
+import { GlobalContext } from '../../lib/context';
+import { lazy, useContext, useRef, useState } from 'react';
+
+export default function RenderNewMeetingText() {
+  const globalContext = useContext(GlobalContext);
+  const state = useThree();
+  const text = 'NEW MEETING';
+  let textMesh = useRef(null);
+  let geometry;
+  // let intensity = 2.5
+  const [intensity, setIntensity] = useState(true);
+
+  // const train = lazy(() => import('/fonts/Train One_Regular.json?url'));
+
+  const loader = new FontLoader();
+  loader.load('/fonts/Train One_Regular.json', function (font) {
+    // loader.load(train, function (font) {
+    geometry = new TextGeometry(text, {
+      font: font,
+      size: 0.14,
+      height: 0.12,
+    });
+
+    textMesh.current = new Mesh(geometry, [
+      new MeshStandardMaterial({
+        emissive: '#2dfff8',
+        emissiveIntensity: globalContext.isSignedIn && intensity ? 10 : 0,
+        toneMapped: false,
+      }),
+      new MeshStandardMaterial({ color: '#2dfff8' }),
+    ]);
+
+    state.scene.add(textMesh.current);
+
+    textMesh.current.position.set(6.99, 4.67, -2.61);
+  });
+
+  // useEffect(() => {}, [globalContext.isSignedIn]);
+
+  // useEffect(() => {
+  //   if(!intensity) {
+
+  //     setTimeout(() => {
+  //       setIntensity(true)
+  //     }, 500)
+  //     // setIntensity(false)
+  //   }
+  // }, [intensity])
+
+  // useEffect(() => {
+  //   if(!globalContext.isSignedIn) {
+  //    let interval =  setInterval(() => {
+  //       setIntensity((prev) => !prev)
+  //     }, 1000)
+  //   }
+
+  //   return () => {
+  //     clearInterval(interval)
+  //   }
+  // }, [])
+  return <></>;
+}
