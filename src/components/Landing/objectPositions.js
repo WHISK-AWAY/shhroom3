b4d8ed7952a936841c13daf743f6fa656156f4c3
Patrick Bryan <pbryan9@gmail.com>
2023-08-22 10:25:40 -0700

    tidy / organize object positions file

--- src/components/Landing/objectPositions.js
+++ src/components/Landing/objectPositions.js
@@ -2,52 +2,6 @@ import { Vector3 } from 'three';
 
 export const objectPositions = {
   initPosition: {
-    // camPosition: new Vector3(
-    //   13.236447409166384,
-    //   3.7209397708118264,
-    //   7.464389431872212,
-    // ),
-    camPosition: new Vector3(
-      9.882191351251901,
-      2.945545047901194,
-      4.347200834586175,
-    ),
-    targetPosition: new Vector3(
-      1.8900082175975619,
-      2.7284963847587003,
-      -3.06286117925677,
-    ),
-    // targetPosition: new Vector3(
-    //   7.008978578979336,
-    //   2.854308301873602,
-    //   1.0516778801734505,
-    // ),
-  },
-  monitor: {
-    camPosition: new Vector3(
-      4.698239424611961,
-      3.187951515813853,
-      1.3237225403016146,
-    ),
-    targetPosition: new Vector3(3.54909, 3.20587, 2.15376),
-  },
-  newMeeting: {
-    camPosition: new Vector3(
-      7.7084461220869285,
-      3.760683379409692,
-      1.4263831780533351,
-    ),
-    targetPosition: new Vector3(7.71108, 3.65457, -2.58681),
-  },
-  corkboard: {
-    camPosition: new Vector3(
-      4.6169697324253685,
-      4.269023824694518,
-      1.8626983480977797,
-    ),
-    targetPosition: new Vector3(3.0905, 4.29743, 1.83965),
-  },
-  initPosition: {
     camPosition: new Vector3(
       13.272240226514993,
       3.643396467464836,
@@ -59,6 +13,25 @@ export const objectPositions = {
       -3.06286117925677,
     ),
   },
+
+  monitor: {
+    camPosition: new Vector3(
+      4.698239424611961,
+      3.187951515813853,
+      1.3237225403016146,
+    ),
+    targetPosition: new Vector3(3.54909, 3.20587, 2.15376),
+  },
+
+  newMeeting: {
+    camPosition: new Vector3(
+      7.7084461220869285,
+      3.760683379409692,
+      1.4263831780533351,
+    ),
+    targetPosition: new Vector3(7.71108, 3.65457, -2.58681),
+  },
+
   newMeetingTunnelZoom: {
     camPosition: new Vector3(
       7.860247276910664,
@@ -67,14 +40,15 @@ export const objectPositions = {
     ),
     targetPosition: new Vector3(7.71108, 3.65457, -2.98681),
   },
-  // sting: {
-  //   camPosition: new Vector3(
-  //     8.744054639054083,
-  //     3.544506309064059,
-  //     -2.1159567013174625,
-  //   ),
-  //   targetPosition: new Vector3(),
-  // },
+
+  corkboard: {
+    camPosition: new Vector3(
+      4.6169697324253685,
+      4.269023824694518,
+      1.8626983480977797,
+    ),
+    targetPosition: new Vector3(3.0905, 4.29743, 1.83965),
+  },
 
   sting: {
     camPosition: new Vector3(
@@ -110,12 +84,4 @@ export const objectPositions = {
       -2.1562431403828706,
     ),
   },
-  // shelves: {
-  //   camPosition: new Vector3(
-  //     5.847438121775274,
-  //     4.851494562168459,
-  //     0.741833545971884,
-  //   ),
-  //   targetPosition: new Vector3(),
-  // },
 };
