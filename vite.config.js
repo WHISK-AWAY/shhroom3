a53ec3c908eb55eb26b2c984fef9e07b62887c20
Patrick Bryan <pbryan9@gmail.com>
2023-09-28 20:04:53 -0700

    add emptyoutdir to build instructions

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
   build: {
+    emptyOutDir: true,
     minify: 'terser',
     terserOptions: {
       format: {
