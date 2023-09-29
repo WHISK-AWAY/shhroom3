07b677a2309e6f2fdba676395e22e11543629c42
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-29 19:16:49 -0400

    add some width and height adh to chat container based ont he state of the user menu being open

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -82,8 +82,10 @@ export default function Chat(props) {
   return (
     <div
       className={`${
-        isUserControlsOpen ? 'w-[60%] max-h-[30dvh] h-[60dvh]' : 'w-[80%]'
-      } border border-blue-700 chat-area-wrapper justify-between  max-h-[30dvh] h-[30dvh] flex flex-col mx-auto font-vt text-[1.5vw] bg-teal-400/20 rounded-md mb-8 3xl:text-[1.1vw] `}
+        isUserControlsOpen
+          ? 'w-[60%] 3xl:w-[50%] 4xl:w-[40%] max-h-[20dvh] h-[50dvh] 5xl:w-[35%] short:w-[50%]'
+          : 'w-[70%] 3xl:w-[60%] 4xl:w-[50%] 5xl:w-[35%] short:w-[50%]'
+      } chat-area-wrapper justify-between  max-h-[30dvh] h-[28dvh] flex flex-col mx-auto font-vt text-[1.5vw] bg-teal-400/10 rounded-md mb-8 3xl:text-[1.1vw] `}
     >
       <div className='flex flex-row h-full items-center justify-center rounded-t-md scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-slate-700 scrollbar-track-slate-800 w-full mx-auto py-3  flex-grow-0 flex-shrink-0'>
         <div
@@ -95,10 +97,10 @@ export default function Chat(props) {
             messageList.map(({ username, timestamp, message }) => (
               <div key={timestamp} className='w-full'>
                 <p>
-                  <span className='font-semibold text-teal-300'>
+                  <span className='font-semibold text-teal-300 text-[1rem] xl:text-[1.1rem] 5xl:text-[1.3rem] 6xl: 6xl:text-[1.4rem]'>
                     {username}{' '}
                   </span>
-                  <span className='text-[.9vw] text-slate-400 ml-1'>
+                  <span className='text-[.9vw] 4xl:text-[.9rem] text-slate-400 ml-1'>
                     {new Date(timestamp).toLocaleString(undefined, {
                       hour: 'numeric',
                       minute: 'numeric',
@@ -106,7 +108,7 @@ export default function Chat(props) {
                     })}
                   </span>
                 </p>
-                <p className='text-slate-300 w-full whitespace-normal break-words'>
+                <p className='text-slate-300 w-full whitespace-normal break-words text-[1rem] 5xl:text-[1.1rem]'>
                   {message}
                 </p>
               </div>
@@ -118,12 +120,12 @@ export default function Chat(props) {
         <button type='submit'>
           <img
             src={plane}
-            alt='paper plane image'
-            className='absolute right-2 top-0 w-[35px] 6xl:w-[55px]'
+            alt='Send message icon'
+            className='absolute right-2 top-1 w-[25px] 2xl:w-[30px]  5xl:w-[45px]'
           />
         </button>
         <input
-          className='px-2 caret-teal-200  text-white bg-teal-500/70 border-[.2px] border-white w-full min-h-[35px] 5xl:h-[40px] 6xl:h-[55px] rounded-b-md placeholder-slate-100 focus:outline-none focus:border-2 whitespace-normal placeholder:break-words flex flex-wrap '
+          className='px-3 caret-teal-200  text-white bg-teal-500/70  w-full min-h-[35px] 2xl:h-[40px]  5xl:h-[55px] rounded-b-md placeholder-slate-100 focus:outline-none  whitespace-normal placeholder:break-words flex flex-wrap text-[1rem] 5xl:text-[1.1rem]'
           type='text'
           name='chat'
           id='chat'
