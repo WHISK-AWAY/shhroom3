9eea72c39ba486fe5c5c238b6a3d5283b4310034
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-29 19:17:57 -0400

    add resp styles for short

--- src/components/RoomUserControls.jsx
+++ src/components/RoomUserControls.jsx
@@ -126,13 +126,13 @@ export default function RoomUserControls({
         ref={mainContainerRef}
         className={` ${
           isUserControlsOpen ? 'opacity-100' : 'opacity-0'
-        } room-controls-wrapper absolute top-[3%] left-0  h-[60vh]  w-[15%] max-w-[300px]  font-vt flex z-50  justify-center rounded-md  text-white text-[1.4vw] 3xl:text-[1vw] overflow-hidden`}
+        } room-controls-wrapper absolute top-[3%] left-0  h-[60vh]  w-[15%] max-w-[300px]  font-vt flex z-50  justify-center rounded-md  text-white text-[1.4vw]  overflow-hidden`}
       >
         <ul className='room-controls-list  flex flex-col gap-10 justify-center  w-full bg-neutral-700/90 h-full p-4 rounded-r-sm'>
           <div
             ref={svgRef}
             className={` 
-            text-[1.1vw] 6xl:text-[.9vw] flex flex-col gap-6  w-[95%] 3xl:w-[80%] self-center`}
+            text-[1.1vw] 3xl:text-[.9vw] 6xl:text-[.7vw] flex flex-col gap-6  w-[95%] 3xl:w-[80%] self-center`}
           >
             <button
               className='flex flex-col items-center'
@@ -144,7 +144,7 @@ export default function RoomUserControls({
               <img
                 src={copy}
                 alt='Copy invitation link icon'
-                className='w-[50%] 5xl:w-[40%] short:w-[40%] transition-all duration-300 hover:scale-[1.2] '
+                className='w-[50%] 4xl:w-[40%] short:w-[35%] transition-all duration-300 hover:scale-[1.2] '
               />
               <li className='link-invite  pt-1'>
                 {isLinkCopied
@@ -161,7 +161,7 @@ export default function RoomUserControls({
                 <img
                   src={chat}
                   alt='Chat icon'
-                  className='w-[65%] 5xl:w-[55%] short:w-[55%]  transition-all duration-300 hover:scale-[1.2] '
+                  className='w-[65%] 4xl:w-[55%] short:w-[45%]  transition-all duration-300 hover:scale-[1.2] '
                 />
                 <li className='link-invite pt-1'>chat</li>
               </button>
@@ -174,7 +174,7 @@ export default function RoomUserControls({
               <img
                 src={door}
                 alt='Leave meeting icon'
-                className='w-[65%] 5xl:w-[55%] short:w-[55%]  transition-all duration-300 hover:scale-[1.2] '
+                className='w-[65%] 4xl:w-[55%] short:w-[45%]  transition-all duration-300 hover:scale-[1.2] '
               />
               <li className='link-invite pt-1'>leave meeting</li>
             </button>
