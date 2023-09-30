ba77df16756e718127c04e72cbb57c6c1fdb366d
Patrick Bryan <pbryan9@gmail.com>
2023-09-30 12:10:48 -0700

    update deploy script to exit upon error

--- deploy.sh
+++ deploy.sh
@@ -5,23 +5,43 @@ getopts c: flag;
 remote_host=pna;
 remote_dest=/var/www/shhroom/html/;
 
+log=deploy.log
+
 if [ $flag = c ]
   then
     remote_host=${OPTARG};
 fi
 
-echo "Establishing ssh connection to $remote_host to set permissions. You'll be asked for your remote sudo password." | tee deploy.log;
+echo "Establishing ssh connection to $remote_host to set permissions. You'll be asked for your remote sudo password." | tee $log;
 
-ssh -t "$remote_host" /srv/permissions.sh | tee -a deploy.log;
+ssh -t "$remote_host" /srv/permissions.sh;
 
-echo "Permissions set. Running build..." | tee -a deploy.log;
+if [ $? -ne 0 ]
+then
+  echo "Error setting permissions. Exiting..." | tee -a $log
+  exit 1;
+fi
 
-npm run build | tee -a deploy.log;
+echo "Permissions set. Running build..." | tee -a $log;
 
-echo "Build complete. Uploading files..." | tee -a deploy.log;
+npm run build;
 
-rsync --verbose --recursive --update --size-only --mkpath --delete --delete-delay dist/ $remote_host:$remote_dest | tee -a deploy.log;
+if [ $? -ne 0 ]
+then
+  echo "Error encountered during build process. Exiting..." | tee -a $log
+  exit 2;
+fi
 
-echo "Done!" | tee -a deploy.log;
+echo "Build complete. Uploading files..." | tee -a $log;
+
+rsync --verbose --recursive --update --size-only --mkpath --delete --delete-delay dist/ $remote_host:$remote_dest;
+
+if [ $? -ne 0 ]
+then
+  echo "Error encountered during remote sync. Exiting..." | tee - a $log
+  exit 3;
+fi
+
+echo "Done!" | tee -a $log;
 
 exit 0;
