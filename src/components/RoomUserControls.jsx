54bce0aa550f20382c80ea599239c826a5af03d3
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-30 13:58:36 -0400

    add resp styles

--- src/components/RoomUserControls.jsx
+++ src/components/RoomUserControls.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect, useRef } from 'react';
-import test from '/svg/esc_button.svg';
 import chat from '/svg/chat.svg';
 import door from '/svg/door.svg';
 import copy from '/svg/copy.svg';
 import arrowDown from '/svg/arrowDown.svg';
 import { gsap } from 'gsap';
-import { is } from 'date-fns/locale';
 
 export default function RoomUserControls({
   roomId,
@@ -26,7 +24,6 @@ export default function RoomUserControls({
   const [isLinkCopied, setIsLinkCopied] = useState(false);
   const previousModeRef = useRef(null);
 
-  // console.log(partnerPeerId);
 
   async function copyToClipboard(text) {
     try {
@@ -37,7 +34,6 @@ export default function RoomUserControls({
     }
   }
 
-  // console.log('shh', thisShhroomer)
 
   useEffect(() => {
     if (isUserControlsOpen) {
@@ -45,7 +41,8 @@ export default function RoomUserControls({
       const ctx = gsap.context(() => {
         const tl = gsap.timeline();
 
-        tl.set(mainContainerRef.current, { height: 0 });
+        tl.set(mainContainerRef.current, { height: 0});
+
         tl.to(topControlsRef.current, {
           width: '15%',
           duration: 0.2,
@@ -54,15 +51,17 @@ export default function RoomUserControls({
           .to(arrowRef.current, {
             rotation: -90,
             duration: 0.3,
+          }).to(mainContainerRef.current, {
+            opacity: 100, 
           })
           .from(mainContainerRef.current, {
             height: 0,
             duration: 0.6,
             ease: 'expo.inOut',
-            opacity: 0,
-          })
-          .to(svgRef.current, {
             // opacity: 100,
+          }, ',')
+          .to(svgRef.current, {
+            opacity: 100,
             duration: 0.8,
             ease: 'power1',
           }, '<');
@@ -76,27 +75,35 @@ export default function RoomUserControls({
       if (!isUserControlsOpen && !previousModeRef.current) {
         const ctx = gsap.context(() => {
           const tl = gsap.timeline({});
+          tl.set(mainContainerRef.current,  {opacity: 100})
 
           tl.from(arrowRef.current, {
             rotation: -90,
             duration: 0.3,
           })
-          .to(mainContainerRef.current, {
-            height: 0,
-            duration: 0.6,
-            ease: 'expo.inOut',
-            opacity: 100,
-          })
-          .from(svgRef.current, {
-            opacity: 100,
-            duration: 0.8,
-            ease: 'power1',
-          }, '<')
-          .from(topControlsRef.current, {
-            width: '15%',
-            duration: 0.2,
-            ease: 'expo',
-          })
+            .to(mainContainerRef.current, {
+              opacity: 100,
+            })
+            .to(mainContainerRef.current, {
+              height: 0,
+              duration: 0.6,
+              ease: 'expo.inOut',
+              // opacity: 100,
+            })
+            .from(
+              svgRef.current,
+              {
+                opacity: 100,
+                duration: 0.8,
+                ease: 'power1',
+              },
+              '<',
+            )
+            .from(topControlsRef.current, {
+              width: '15%',
+              duration: 0.2,
+              ease: 'expo',
+            });
         });
 
         previousModeRef.current = true;
@@ -111,7 +118,7 @@ export default function RoomUserControls({
     <>
       <div
         ref={topControlsRef}
-        className='  w-[2%] max-w-[300px] h-[3%] flex justify-end items-center top-2 absolute bg-teal-400/90 z-[99] rounded-sm 5xl:w-[1%] '
+        className='  w-[2%] 6xl:max-w-[330px] max-w-[300px] h-[3%] flex justify-end items-center top-2 absolute bg-teal-400/90 z-[99] rounded-sm 5xl:w-[1%] '
       >
         <img
           onClick={() => setIsUserControlsOpen((prev) => !prev)}
@@ -124,13 +131,13 @@ export default function RoomUserControls({
 
       <div
         ref={mainContainerRef}
-        className={`  room-controls-wrapper absolute top-[3%] left-0  h-[60vh]  w-[15%] max-w-[300px]  font-vt flex z-50  justify-center rounded-md  text-white text-[1.4vw]  overflow-hidden`}
+        className={`  room-controls-wrapper absolute top-[3%] left-0  h-[60vh]  w-[15%] 6xl:max-w-[330px] max-w-[300px]  font-vt flex z-50  justify-center rounded-md  text-white text-[1.4vw]  overflow-hidden bg-neutral-700/90 opacity-0`}
       >
-        <ul className='room-controls-list  flex flex-col gap-10 justify-center  w-full bg-neutral-700/90 h-full p-4 rounded-r-sm'>
+        <ul className='room-controls-list  flex flex-col gap-10 justify-center  w-full  h-full p-4 rounded-r-sm'>
           <div
             ref={svgRef}
             className={` 
-            text-[1.1vw] 3xl:text-[.9vw] 6xl:text-[.7vw] flex flex-col gap-6  w-[95%] 3xl:w-[80%] self-center`}
+            text-[1.1vw] opacity-0 3xl:text-[.9vw] 6xl:text-[.7vw] flex flex-col gap-6  w-[95%] 3xl:w-[80%] self-center`}
           >
             <button
               className='flex flex-col items-center'
